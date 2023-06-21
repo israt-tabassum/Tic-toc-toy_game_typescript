287d579ade5a33ae443eac4d1af1a00ae7376058
israt-tabassum <67886851+israt-tabassum@users.noreply.github.com>
2023-06-21 14:46:41 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import User from "./components/User";
+import MyButton from "./components/MyButton";
+import Image from "./components/Image";
+import List from "./components/List";
+import { Practicerender } from './components/Practicerender';
+import Pevents from './components/Pevents';
+import Screenupdate from './components/Screenupdate';
+import CountTogether from './components/CountTogether';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Typescript Practice</h1>
+      <p>Initial practice of react: <User name="ansar" age={32} />
+      <User name="jhon" age={31} /> </p>
+      <p>Button in react: <MyButton/> </p>
+      <br/>
+      <br/>
+      <p>Image in react: <Image/> </p> 
+      <p>Listing in react: <List/></p> 
+      <p>Rendering in react: <Practicerender/></p> 
+      <p>Event Handling in react: <Pevents/></p> 
+      <p><Screenupdate/></p> 
+      <p><CountTogether/></p> 
     </div>
   );
 }
